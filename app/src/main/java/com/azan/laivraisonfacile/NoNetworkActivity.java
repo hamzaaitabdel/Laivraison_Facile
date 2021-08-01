f8e8846465e83aa0aa71f8253c48cd294625a627
abdellah711 <alaouiabdellah711@gmail.com>
2021-08-01 20:28:46 +0100

    NoNetworkActivity

--- app/src/main/java/com/azan/laivraisonfacile/NoNetworkActivity.java
+++ app/src/main/java/com/azan/laivraisonfacile/NoNetworkActivity.java
@@ -1,15 +1,46 @@
 package com.azan.laivraisonfacile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class NoNetworkActivity extends AppCompatActivity {
     private WebView webView;
+    SwipeRefreshLayout swipeRefresh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_network);
+        webView = findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/pas_de_connexion.htm");
+
+        swipeRefresh = findViewById(R.id.refreshLayout);
+
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                ConnectivityManager manager =(ConnectivityManager) getApplicationContext()
+                        .getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo activeNetwork = manager.getActiveNetworkInfo();
+                if (null != activeNetwork) {
+                    if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI || activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                        swipeRefresh.setRefreshing(false);
+                        Intent i = new Intent(NoNetworkActivity.this, MainActivity.class); startActivity(i);
+                        finish();
+                    }
+                }
+                        swipeRefresh.setRefreshing(false);
+            }
+        });
     }
 }
